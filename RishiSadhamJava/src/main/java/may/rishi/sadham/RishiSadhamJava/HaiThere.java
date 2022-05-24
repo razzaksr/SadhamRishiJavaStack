86487dbf49853aa2151517ce8f133032360e502f
razzaksr <razzaksr@gmail.com>
2022-05-24 12:13:03 +0530

    Basic program

---
+++ RishiSadhamJava/src/main/java/may/rishi/sadham/RishiSadhamJava/HaiThere.java
@@ -0,0 +1,7 @@
+package may.rishi.sadham.RishiSadhamJava;
+
+public class HaiThere {
+	public static void main(String[] args) {
+		System.out.println("Welcome to Zealous Tech Corp to learn Java Stack");
+	}
+}
---
+++ RishiSadhamJava/src/main/java/may/rishi/sadham/RishiSadhamJava/Showing.java
@@ -0,0 +1,12 @@
+package may.rishi.sadham.RishiSadhamJava;
+
+public class Showing 
+{
+	public static void main(String[] args) 
+	{
+		String modelName="Boat AirDopes139";
+		Integer version=4,price=1299;
+		
+		System.out.println("My Headset details: name is "+modelName+" bluetooth version is "+version+" bought cost is "+price);
+	}
+}
